408a1c981495e558e60910eabcd13c9f33fa3708
github username <github email address>
2020-05-05 17:16:35 -0400

    add controller actions in marathons_controller

--- app/controllers/marathons_controller.rb
+++ app/controllers/marathons_controller.rb
@@ -3,9 +3,35 @@ class MarathonsController < ApplicationController
     # index, new, show, edit
 
     get '/marathons' do 
-        erb :"marathons/index"
+        erb :'marathons/index'
     end 
 
+    get '/marathons/new' do
+        erb :'marathons/new'
+    end 
 
+    post '/marathons' do 
+
+    end
+
+    get '/marathons/:id' do 
+        # params to find use params[:id]
+        erb :"marathons/show"
+    end 
+
+    get '/marathons/:id/edit' do 
+
+    end 
+
+    patch '/marathons/:id' do 
+
+    end 
+
+    get '/marathons/:id/delete' do
+
+    end 
+
+    delete '/marathons/:id/delete' do
+    end 
 
 end 
