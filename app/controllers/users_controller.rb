b818565b1267cddc8a0f611a395eebba04b48ab7
github username <github email address>
2020-05-06 21:55:56 -0400

    add user erb files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
 
+    get '/users/:id' do 
+
+    end 
+
+    get '/users/sign_up' do 
+    
+    end 
+
+    post '/users/sign_up' do 
+
+    end 
+
+    get '/logout' do 
+
+    end 
+
+    delete '/users/deactivate' do 
+    
+    end 
+
 end
