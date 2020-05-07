ab2e60afb9d5daac95218f55491d05578e0a836f
github username <github email address>
2020-05-06 21:37:23 -0400

    add user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505220626) do
+ActiveRecord::Schema.define(version: 20200507013253) do
 
   create_table "marathons", force: :cascade do |t|
     t.string   "name"
     t.datetime "date"
     t.string   "location"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
